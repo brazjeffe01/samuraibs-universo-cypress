cb723732d34701182ba8859ef4c239a58775b54b
Jefferson Braz <brazjeffe@gmail.com>
2022-04-07 17:54:30 -0300

    Melhorando a organização e implementando o underscore

--- cypress/integration/dash.spec.js
+++ cypress/integration/dash.spec.js
@@ -3,41 +3,27 @@ const { CopyResponse } = require("pg-protocol/dist/messages")
 import loginPage from '../support/pages/login'
 import dashPage from '../support/pages/dash'
 
+import { customer, provider, appointment } from '../support/factories/dash'
+
 describe('dashboard', function() {
 
   context('quando o cliente faz um agendamento no app mobile', function() {
 
-    const data = {
-      customer: {
-        name: 'Nikki Sixx',
-        email: 'sixx@motleycrue.com',
-        password: 'pwd123',
-        is_provider: false
-      },
-      provider: {
-        name: 'Ramon Valdes',
-        email: 'ramon@televisa.com',
-        password: 'pwd123',
-        is_provider: true
-      },
-      appointmentHour: '14:00'
-    }
-
     before(function() {
-      cy.postUser(data.provider)
-      cy.postUser(data.customer)
+      cy.postUser(provider)
+      cy.postUser(customer)
 
-      cy.apiLogin(data.customer)
+      cy.apiLogin(customer)
 
-      cy.setProviderId(data.provider.email)
+      cy.setProviderId(provider.email)
 
-      cy.createAppointment(data.appointmentHour)
+      cy.createAppointment(appointment.hour)
     })
 
     it('o mesmo deve ser exibido no dashboard', function() {
 
       loginPage.go()
-      loginPage.form(data.provider)
+      loginPage.form(provider)
       loginPage.submit()
 
       dashPage.calendarShouldBeVisible()
@@ -45,7 +31,7 @@ describe('dashboard', function() {
       const day = Cypress.env('appointmentDay')
       dashPage.selectDay(day)
 
-      dashPage.appointmentShouldBe(data.customer, data.appointmentHour)
+      dashPage.appointmentShouldBe(customer, appointment.hour)
 
     })
 
