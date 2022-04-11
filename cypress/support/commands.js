69bf06ac1c569c2bef815d62e83a163c4ad7d162
Jefferson Braz <brazjeffe@gmail.com>
2022-04-11 17:15:49 -0300

    Resolvendo questões de data dos agendamentos nos últimos dias do mês e nos finais de semana

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -75,7 +75,7 @@ Cypress.Commands.add('createAppointment', function(hour) {
 
   now.setDate(now.getDate() + 1)
 
-  Cypress.env('appointmentDay', now.getDate())
+  Cypress.env('appointmentDate', now)
 
   const date = moment(now).format(`YYYY-MM-DD ${hour}:00`)
 
